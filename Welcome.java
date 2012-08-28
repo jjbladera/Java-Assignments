8f13b79d897d7058f38a604929eeae6679910580
Javier C <youracow@gmail.com>
2012-08-28 12:45:05 -0600

    adding helloworld

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome{ //welcome class name needs to be the same as fiel name
+	public static void main (String[] args){
+		System.out.println("hello java world");
+	}
+}
+
+//javac 1.6.0_27
